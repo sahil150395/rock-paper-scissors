9d7167f303eb8ad1a7d03a97ce197d6bb5b5bacd
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-20 05:21:08 +0000

    completed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,3 +27,21 @@ scissors = '''
 ---.__(___)
 '''
 
+rps = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if user_choice < 3 and user_choice >= 0:
+    print(rps[user_choice])
+
+    comp_choice = random.randint(0,2)
+    print("Computer chose:")
+    print(rps[comp_choice])
+
+    if user_choice == comp_choice:
+        print("Match Tied")
+    elif (user_choice == 0 and comp_choice == 2) or (user_choice == 2 and comp_choice == 1) or (user_choice == 1 and comp_choice == 0):
+        print("You win")
+    else:
+        print("You lose")
+else:
+    print("Wrong input")
